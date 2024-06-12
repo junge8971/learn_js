751330b3849784c625ea8ed65cea4ff57be14891
junge8971 <junge8971@gmail.com>
2024-06-12 10:01:37 +0300

    Создал базоваое рекат приложение

---
+++ practice/Ulbi/fundamental_react_course/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+// Монтирование компонента в приложение
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
