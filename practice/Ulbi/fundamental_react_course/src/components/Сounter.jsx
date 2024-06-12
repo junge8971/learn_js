0162c656dd0b27f69c16818f7fb42a637e69fb3a
junge8971 <junge8971@gmail.com>
2024-06-12 11:26:46 +0300

    Применён линтер

--- practice/Ulbi/fundamental_react_course/src/components/Сounter.jsx
+++ practice/Ulbi/fundamental_react_course/src/components/Сounter.jsx
@@ -5,25 +5,23 @@ import React, { useState } from "react";
 
 const Counter = () => {
   // Чтобы счётчик менялся при нажатии на копки нужно соопщить реакту, что состояние компонента изменилось и reactdom перерисовал компонент
-    let [count, set_count] = useState(1)
+  let [count, set_count] = useState(1);
 
-    function increment() {
-        // чтобы изменить значение переменной и перерисовать компонент нуно использовать функцию из state
-        set_count(++count);
-      }
-      function decrement() {
-        set_count(--count);
-      }
-      // Написание разметки html внутри js называется jsx
-    return (
-        <div>
-            <h1>{count}</h1>
-            <button onClick={increment}>Инкримент</button>
-            <button onClick={decrement}>Декримент</button>
-        </div>
-    )
-
-}
-
+  function increment() {
+    // чтобы изменить значение переменной и перерисовать компонент нуно использовать функцию из state
+    set_count(++count);
+  }
+  function decrement() {
+    set_count(--count);
+  }
+  // Написание разметки html внутри js называется jsx
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increment}>Инкримент</button>
+      <button onClick={decrement}>Декримент</button>
+    </div>
+  );
+};
 
 export default Counter;
