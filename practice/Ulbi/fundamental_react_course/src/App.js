4c84b2d03c34afda18a25e6a3ceefca318a0d0a4
junge8971 <junge8971@gmail.com>
2024-06-12 10:38:08 +0300

    Создал счётчик с состоянием

--- practice/Ulbi/fundamental_react_course/src/App.js
+++ practice/Ulbi/fundamental_react_course/src/App.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-function App() {
-  return <div className="App">Кек</div>;
+export default function App() {
+  // Написание разметки html внутри js называется jsx
+
+  // Чтобы счётчик менялся при нажатии на копки нужно соопщить реакту, что состояние компонента изменилось и reactdom перерисовал компонент
+  let [counter, set_counter] = useState(0);
+
+  function increment() {
+    // чтобы изменить значение переменной и перерисовать компонент нуно использовать функцию из state
+    set_counter(++counter);
+  }
+  function decrement() {
+    set_counter(--counter);
+  }
+  return (
+    <div className="App">
+      <h1>{counter}</h1>
+      <button onClick={increment}>Инкримент</button>
+      <button onClick={decrement}>Декримент</button>
+    </div>
+  );
 }
-
-export default App;
