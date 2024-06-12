abb235fa2e69f3dc715be749b61f3a4c11af952c
junge8971 <junge8971@gmail.com>
2024-06-12 13:31:11 +0300

    замена root на app

--- practice/Ulbi/fundamental_react_course/src/index.js
+++ practice/Ulbi/fundamental_react_course/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 // Монтирование компонента в приложение
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+const App = ReactDOM.createRoot(document.getElementById("App"));
+App.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
