b29deb44a42a988735d30b0bd1de528493fb5f17
junge8971 <junge8971@gmail.com>
2024-06-12 15:52:55 +0300

    Реализован функционал отрисовка n элементов

--- practice/Ulbi/fundamental_react_course/src/App.js
+++ practice/Ulbi/fundamental_react_course/src/App.js
@@ -7,12 +7,24 @@ import Post from "./components/Post";
 import "./styles/app.css";
 
 export default function App() {
+  let [posts, set_posts] = React.useState([
+    { id: 2, title: "Стартуем 2", body: "Описание 2" },
+    { id: 3, title: "Стартуем 3", body: "Описание 3" },
+  ]);
   return (
     <div className="App">
       <Counter />
       <ClassCounter />
+
+      <h1>Список постов</h1>
       <Post post={{ id: 0, title: "Стартуем", body: "Описание" }} />
       <Post post={{ id: 1, title: "Едем", body: "Описание_2" }} />
+
+      {/* Пример преобразования массива объектов в массив реакт элементов*/}
+      {/* Обяхательно передаём уникальный ключ. Это нужно для рэндеринга*/}
+      {posts.map((post) => (
+        <Post post={post} key={post.id} />
+      ))}
     </div>
   );
 }
