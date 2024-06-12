19a7dd3cc264b70960898b591352df64c57d3cfa
junge8971 <junge8971@gmail.com>
2024-06-12 13:34:04 +0300

    Возращаю root назад

--- practice/Ulbi/fundamental_react_course/src/index.js
+++ practice/Ulbi/fundamental_react_course/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 // Монтирование компонента в приложение
-const app = ReactDOM.createRoot(document.getElementById("App"));
-app.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
