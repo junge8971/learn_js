ebcd693f779f29cad6644b932e83c32ed77ddb16
junge8971 <junge8971@gmail.com>
2024-07-05 12:42:01 +0300

    Создан пустой проект для прохождения курса владлена по реакту

---
+++ practice/vladlen/base_react_course/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p>Ку</p>
+    </>
+  );
+}
+
+export default App;
